1b6e11361926d3573565ed22a32d6e120232fde1
Jobinthomas123 <jobinmthomas7@gmail.com>
2023-05-01 17:16:01 -0500

    - formatting

---
+++ removeFourSpaces.py
@@ -0,0 +1,17 @@
+x = 113
+
+while x < 1136:
+    # Open the file for reading
+    with open(f'songtext/{x}.txt', 'r') as file:
+        # Read the contents of the file
+        contents = file.read()
+
+    # Replace tab spaces with 4.txt space bar spaces
+    new_contents = contents.replace('    ', '')
+
+    # Open the file for writing
+    with open(f'songtext/{x}.txt', 'w') as file:
+        # Write the modified contents back to the file
+        file.write(new_contents)
+
+    x = x + 1
